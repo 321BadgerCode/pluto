992a2408f5c4304a063555aced20e44280b1ee1b
Badger Code <83559316+321BadgerCode@users.noreply.github.com>
2022-04-15 23:33:34 -0400

    Delete script directory

--- script/general/dont_destroy_on_load.cs
+++
@@ -1,8 +0,0 @@
-//badger
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public sealed class dont_destroy_on_load:MonoBehaviour{
-	private void Awake(){DontDestroyOnLoad(gameObject);}
-}
